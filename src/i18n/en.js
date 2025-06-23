7e68253e77fe8560da1d85907448944bcee15804
ygbanzato <ygbanzato@isptgroup.com>
2025-06-23 21:17:50 +0100

    start project

---
+++ src/i18n/en.js
@@ -0,0 +1,4 @@
+export default {
+    greeting: "Welcome to my portfolio",
+    description: "Full Stack Web Developer"
+  };
---
+++ src/i18n/pt.js
@@ -0,0 +1,4 @@
+export default {
+    greeting: "Bem-vindo ao meu portfólio",
+    description: "Desenvolvedor Web Full Stack"
+  };
